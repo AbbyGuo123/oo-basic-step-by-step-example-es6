e9a5ec772cef6a8346a02be027aaba28f7c6c64d
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-09 17:04:43 +0800

    practice_7

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,17 @@
+const Person = require('../../src/practice_7/person');
+const Class = require('../../src/practice_7/class');
+class Student extends Person{
+    constructor(name,age,klass){
+        super(name, age);
+        this.klass = klass;
+    }
+    introduce(){
+        return super.introduce()+' I am a Student. I am at Class '+this.klass.number+'.';
+    }
+}
+module.exports =Student;
+
+
+
+
 
