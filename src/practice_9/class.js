36984434144bae897d1953d58eba6bb3668c007f
ABBY GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUOAB@OOCL.COM>
2018-07-09 19:38:14 +0800

    practice_9

--- src/practice_9/class.js
+++ src/practice_9/class.js
@@ -1,1 +1,21 @@
-
+class Class{
+    constructor(number){
+        this.number = number;
+    }
+    getDisplayName(){
+        return "Class "+this.number;
+    }
+    assignLeader(student){
+        if(student.klass.equal(this))
+            this.leader = student;
+        else
+        console.log('It is not one of us.');
+    }
+    equal(klass){
+        return this.number===klass.number;
+    }
+    appendMember(student){
+        student.klass = this;
+    }
+}
+module.exports = Class;
--- src/practice_9/student.js
+++ src/practice_9/student.js
@@ -1,1 +1,24 @@
+const Person = require('../../src/practice_8/person');
+const Class = require('../../src/practice_8/class');
+class Student extends Person{
+    constructor(id,name,age,klass){
+        super(id,name, age);
+        this.klass = klass;
+    }
+    introduce(){
+        if(this.klass.leader!==undefined){
+            if(this.klass.leader.equal(this))
+            return `${super.introduce()} I am a Student. I am Leader of Class ${this.klass.number}.`;
+        }
+        return `${super.introduce()} I am a Student. I am at Class ${this.klass.number}.`;
+    }
+    equal(student){
+        return student.id===this.id;
+    }
+}
+module.exports =Student;
+
+
+
+
 
